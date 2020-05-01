53770e62ad82fbffa065cfd18bb59454994e9fb3
bingolusa <47233790+yilmazbingo@users.noreply.github.com>
2020-05-01 16:22:34 -0400

    Update auth0.js

--- services/auth0.js
+++ services/auth0.js
@@ -7,8 +7,8 @@ import { getCookieFromReq } from "../helpers/utils";
 class Auth0 {
   constructor() {
     this.auth0 = new auth0.WebAuth({
-      domain: "dev-udiktky2.auth0.com",
-      clientID: "AyjiNJgMky3Q3Qf04XjHwm3YRWtr6GEX",
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH_CLIENT_ID,
       // response_mode: "form_post",
       redirectUri: "http://localhost:3000/callback",
       responseType: "token id_token",
