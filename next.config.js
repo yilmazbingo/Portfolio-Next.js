0a0ea0c699a3c30332a452dde6258e6074066ebe
bingolusa <47233790+yilmazbingo@users.noreply.github.com>
2020-05-01 16:23:24 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+
 const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
-module.exports = withCSS(withSass());
+require("dotenv").config();
+
+const config = {
+  env: {
+    AUTH_CLIENT_ID: process.env.AUTH_CLIENT_ID,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  },
+};
+module.exports = withCSS(withSass(config));
